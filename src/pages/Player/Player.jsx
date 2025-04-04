e3376794fb4a422aae6c92ca42670cc43f579530
Nadeem-pc <nadeempc777@gmail.com>
2025-04-04 20:41:08 +0530

    Added some more features

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useParams } from 'react-router-dom'
 
 const Player = () => {
+
+  const {id} = useParams()
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    type: ""
+  })
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OGNmYTlhMDE5YzFlZDVjOTU1ZTUwM2Q4MDliY2U4NyIsIm5iZiI6MTc0MzY2MTAxMC42MzkwMDAyLCJzdWIiOiI2N2VlMjdkMmI1YjFjNWE0YzNhN2E1ZjQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Ytc46X3-QWQ39mymQLV-MBDFL0K9GiZAMszKO54QsEM'
+    }
+  };
+  
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results[0]))
+    .catch(err => console.error(err));
+  },[])
+
+
   return (
     <div className='player'>
       <img src={back_arrow_icon} alt="" />
       <iframe width='90%' height='90%'
-      src='https://www.youtube.com/embed/BFr6e_WQcXQ'
+      src= {`https://www.youtube.com/embed/${apiData.key}`}
       title='trailer' frameBorder='0' allowFullScreen></iframe>
       <div className="player-info">
-        <p>Published Date</p>
-        <p>Name</p>
-        <p>Type</p>
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
       </div>
     </div>
   )
